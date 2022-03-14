652bd60447adefe9e35d4dbbf0d6e5b7504ba945
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2022-03-14 16:50:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
 Webcam.set({
     width: 350,
     height: 300,
-image_format:'png',
-image_quality:90,
+    image_format: 'png',
+    png_quality: 90
 });
-camera=document.getElementById("camera");
-Webcam.attach('#camera');
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
 
-function take_snapshot()
-{
-    Webcam.snap(function(data_uri) {
-     document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="image_captured" src="'+data_uri+'"/>';
     });
+};
+
+console.log("ml5 Version: ",ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/BMzs-E2sD/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded Successfully!");
+};
+
+function check() {
+    img = document.getElementById("image_captured");
+    classifier.classify(img, gotResults);
 }
 
-console.log("ML5 Version", ml5.version);
-
-classifier=imageClassifier('https://teachablemachine.withgoogle.com/models/BMzs-E2sD/model.json', modelLoaded);
-
-function modelLoaded()
-{
-    console.log("Model Loaded");
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_of_objects").innerHTML = results[0].label;
+        document.getElementById("result_of_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
